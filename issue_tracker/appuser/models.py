0ac8121a54eefdbcde04c64773ec135dcc39f14b
Karzhaubay Yerbolat <karzhaubayerbolat@gmail.com>
2024-01-23 11:09:55 +0600

    Fix issues in appuser models

--- issue_tracker/appuser/models.py
+++ issue_tracker/appuser/models.py
@@ -4,12 +4,23 @@ from django.db import models
 
 class AppUser(AbstractUser):
     phone = models.CharField(max_length=100, verbose_name="Phone", blank=True)
-    photo = models.ImageField(upload_to="users/photos", verbose_name="Photo", blank=True)
+    photo = models.ImageField(upload_to="users/images", verbose_name="Image", blank=True)
     department = models.CharField(max_length=100, verbose_name='Department', blank=True)
     job_title = models.CharField(max_length=100, verbose_name='Job Title', blank=True)
-    team = models.ForeignKey('appuser.Team', on_delete=models.PROTECT, verbose_name='Team', null=True)
+    team = models.ForeignKey(
+        'appuser.Team',
+        on_delete=models.PROTECT,
+        verbose_name='Team',
+        null=True,
+        related_name='users'
+    )
 
 
 class Team(models.Model):
     title = models.CharField(max_length=100, verbose_name='Title')
-    team_lead = models.OneToOneField('appuser.AppUser', on_delete=models.PROTECT, verbose_name='Teamlead')
+    team_lead = models.OneToOneField(
+        'appuser.AppUser',
+        on_delete=models.PROTECT,
+        verbose_name='Team leader',
+        related_name='leading_team'
+    )
